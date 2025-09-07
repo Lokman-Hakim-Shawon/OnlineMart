c53eea92929698e5cf08fa394c25cc4961bf85ec
lokman hakim <lokmanhakimshawon3@gmail.com>
2025-09-07 22:38:58 +0600

    create order page

---
+++ src/pages/homePage/paymentPage/CartSection.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const CartSection = ({cart}) => {
+    console.log('data of cart',cart.id)
+    return (
+        <div>
+            <h1 className='text-3xl font-bold p-2 bg-primaryColor text-white text-center'>Cart Details</h1>
+            <table className='w-full text-center'>
+                <thead className=''>
+                <tr className="">
+                    <th className='border-2'>Image</th>
+                    <th className='border-2'>Name</th>
+                    <th className='border-2'>Price</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td className='border-2 h-24 w-24'><img src={cart.img} alt="image" /></td>
+                    <td className='border-2'>{cart.product_name}</td>
+                    <td className='border-2'>{cart.price}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default CartSection;
--- src/pages/homePage/paymentPage/PaymentPage.jsx
+++ src/pages/homePage/paymentPage/PaymentPage.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PaymentAddress from './PaymentAddress';
+import CartSection from './CartSection';
+import { useParams } from 'react-router-dom';
 
 const PaymentPage = () => {
+    const {id} =useParams()
+    const [data,setData]=useState(null)
+    useEffect(()=>{
+        fetch('/ProductSectionData.json')
+        .then(res=>res.json())
+        .then(data=>{
+            const selected=data.find(item=>item.id == id)
+            setData(selected)
+        })
+    },[])
+    if(!data){
+        return <p>loading ....</p>
+    }
+    console.log(data)
+    
     return (
         <div className='mt-8 pt-8 grid grid-1'>
-            <div className='flex flex-col lg:flex-row justify-between items-center space-x-8'>
+            <div className='flex flex-col lg:flex-row justify-between items-center space-y-8 lg:space-y-0 lg:space-x-8'>
                 <div className='flex-1 border-2 border-primaryColor lg:h-[80vh]'><PaymentAddress/></div>
-                <div className='flex-1 border-2 h-[80vh]'>section 2</div>
+                <div className='flex-1 border-2 h-[80vh] w-full'><CartSection cart={data} /></div>
             </div>
             <div> row 2</div> 
         </div>
